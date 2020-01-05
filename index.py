ccbedde8ce74bf2328cd19e764ce257c97da0abd
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2020-01-05 22:53:39 +0800

    sorting dan topten

--- index.py
+++ index.py
@@ -1,11 +1,32 @@
 import json
-
-with open('data.json') as f:
+resultcount =[]
+with open('datatest.json') as f:
     data = json.load(f)
     for i in data:
-      print('======================')
-      print('commit ', len(i['comments']))
-      print('like ', i['likes']) 
-      print(i['img_urls'])
-      print('======================')
-# print(len(data))
+      if 'comments' in i.keys():
+        databaru ={
+          "img_urls":i['img_urls'],
+          "comments":len(i['comments']),
+          "datetime":i['datetime'],
+          "caption":i['caption'],
+          "likes":i['likes']
+        }
+      else:
+        databaru ={
+          "img_urls":i['img_urls'],
+          "comments":0,
+          "datetime":i['datetime'],
+          "caption":i['caption'],
+          "likes":i['likes']
+        }      
+      resultcount.append(databaru)
+
+resultcount.sort(key=lambda x: x["comments"],reverse=True)
+with open('hasil-sorting.json', 'w') as outfile:
+    json.dump(resultcount, outfile)
+topten=[]
+for i in range(10):
+   topten.append(resultcount[i])
+
+with open('topten.json', 'w') as t:
+    json.dump(topten, t)
